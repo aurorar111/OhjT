a5dbf5d739042a27d40519df4e8c45c10c2102ba
crin <cerineh@uef.fi>
2025-04-28 10:07:30 +0300

    kkkkk

---
+++ src/main/java/com/example/ohjt/Tiedosto.java
@@ -0,0 +1,33 @@
+package com.example.ohjt;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Tiedosto {
+
+    public void tallennaTiedostoon (){
+        try {
+            File tiedosto = new File("tiedosto.txt");
+            if (!tiedosto.exists()) {
+                tiedosto.createNewFile();
+            }
+            FileWriter kTiedosto = new FileWriter("tiedosto.txt", true);
+            kTiedosto.write();
+            kTiedosto.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void lueTiedosto (){
+        try {
+            File LTiedosto = new File("tiedosto.txt");
+            Scanner lukija = new Scanner(LTiedosto);
+            while (lukija.hasNextLine()) {
+                String tiedot  = lukija.nextLine();
+                lukija.close();
+            }
+        }catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
