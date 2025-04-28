eb36d789c5cbf418844d793dbc3bb4a48d409158
lottaraisanen <lottarai@uef.fi>
2025-04-28 10:00:26 +0300

    .

--- src/main/java/com/example/ohjt/TestiLuokka.java
+++ src/main/java/com/example/ohjt/TestiLuokka.java
@@ -3,6 +3,8 @@ package com.example.ohjt;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+//toimiiko
+
 public class TestiLuokka extends Application {
 
     public static void main(String[] args) {
