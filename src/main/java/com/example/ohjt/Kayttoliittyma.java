6d26dc282b84187c4eae28ade9871c325843d2dd
helmiehrukainen <helmie@uef.fi>
2025-05-07 09:59:06 +0300

    päivitys

--- src/main/java/com/example/ohjt/Kayttoliittyma.java
+++ src/main/java/com/example/ohjt/Kayttoliittyma.java
@@ -155,11 +155,12 @@ public class Kayttoliittyma extends Application {
         taulukko.setPlaceholder(new Label("Ei vielä tietoja"));
         taulukkoMaksut.setStyle("-fx-background-color:#D5E5D5;");
         taulukkoMaksut.setPlaceholder(new Label ("Ei vielä tietoja"));
-        //toimiikoonyt
+        //toimiikoo nyt
 
     }
     public static void main(String[] args) {
         launch(args);
     }
+
 }
 
