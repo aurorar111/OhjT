68481c040cc7f7b3c144c60484328bf3e636808b
tildaruonamo <aurorar@uef.fi>
2025-05-13 17:28:40 +0300

    public

--- src/main/java/com/example/ohjt/Tiedosto.java
+++ src/main/java/com/example/ohjt/Tiedosto.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Tiedosto extends Kayttoliittyma {
 
-    private OlioLuokka olioLuokka = new OlioLuokka();
+    public OlioLuokka olioLuokka = new OlioLuokka();
 
     public void tallennaTiedostoon (){
         try {
