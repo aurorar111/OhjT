90820f24a86b9af9ec8e2c149929dfb87633b7fe
tildaruonamo <aurorar@uef.fi>
2025-05-13 17:45:32 +0300

    tiedostoon tallemmus

--- src/main/java/com/example/ohjt/Tiedosto.java
+++ src/main/java/com/example/ohjt/Tiedosto.java
@@ -16,12 +16,13 @@ public class Tiedosto extends Kayttoliittyma {
             }
             FileWriter kTiedosto = new FileWriter("tiedosto.txt", true);
             kTiedosto.write(olioLuokka.getHenkilokuntaID());
+            kTiedosto.write(tfAsiakasID.getText());
             kTiedosto.write("Moi");
 
             kTiedosto.close();
             System.out.println("Tiedosto tallennettu");
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            
         }
     }
     public void lueTiedosto (){
