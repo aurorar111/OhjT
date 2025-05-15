fe596cb84fc4235c412d9a39a265cf7165d2458d
lottaraisanen <lottarai@uef.fi>
2025-05-15 18:37:56 +0300

    lisätty jtn

--- src/main/java/com/example/ohjt/Tietokanta.java
+++ src/main/java/com/example/ohjt/Tietokanta.java
@@ -22,7 +22,7 @@ public class Tietokanta {
     public static void lisaaVarausTietokantaan(String asiakasNimi, String asiakasGmail, String asiakasPuh, String asiakasSyntyma, String asiakasID) {
         String sql = "INSERT INTO asiakas(nimi, sähköposti, puhelinnumero, syntymäaika, asiakas_id) VALUES (?,?,?,?,?)";
         String sql2 = "INSERT INTO varaus(asiakas_id, varaus_alku, varaus_loppu, varaus_id, järjestelmä_id, mökki_id, lasku_id) VALUES (?,?,?,?,?,?,?)";
-        String sql3 = "INSERT INTO mökit (hinta, mökkitaso, mökki_id, järjestelmä_id) VALUES (?,?,?,?)";
+        String sql3 = "INSERT INTO mökit (hinta, saatavuus, mökkitaso, mökki_id, järjestelmä_id) VALUES (?,?,?,?,?)";
         //saatavuus
         try (Connection conn = connect();
              PreparedStatement statement1 = conn.prepareStatement(sql)) {
@@ -44,9 +44,9 @@ public class Tietokanta {
             PreparedStatement statement3 = conn.prepareStatement(sql3);
             statement3.setString(1, Kayttoliittyma.getVarauksenHinta());
             statement3.setString(2, String.valueOf(Kayttoliittyma.getSaatavuus()));
-            statement3.setString(2, Kayttoliittyma.getMokkiTaso());
-            statement3.setString(3, String.valueOf(Kayttoliittyma.getMokkiID()));
-            statement3.setString(4, String.valueOf(Kayttoliittyma.getJarjestelmaID()));
+            statement3.setString(3, Kayttoliittyma.getMokkiTaso());
+            statement3.setString(4, String.valueOf(Kayttoliittyma.getMokkiID()));
+            statement3.setString(5, String.valueOf(Kayttoliittyma.getJarjestelmaID()));
 
             // PÄIVITÄ VIELÄ MAKSUT, lasku_id, maksujen_tila, järjestelmä_id)
 
